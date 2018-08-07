e64c2a784b96d2a58a9d902e07bd18f2c1e22394
rochellesagh <rsrochelli@gmail.com>
2018-08-07 19:23:03 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -11,42 +11,42 @@ simple_curfew_checker(11)
 simple_curfew_checker(12)
 
 
-def curfew_checker(time)
-  if time >= 11
-    puts "You're in trouble! Better get home quick!"
-  end
-  if time < 11 
-    puts "Keep having fun!"
-  end
-end 
-curfew_checker(10)
-curfew_checker(11)
-curfew_checker(12)
+# def curfew_checker(time)
+#   if time >= 11
+#     puts "You're in trouble! Better get home quick!"
+#   end
+#   if time < 11 
+#     puts "Keep having fun!"
+#   end
+# end 
+# curfew_checker(10)
+# curfew_checker(11)
+# curfew_checker(12)
 
-def complex_curfew_checker(time)
-  if time == 11
-    puts "Time to apparate!"
-  end 
-  if time > 11
-    puts "You're in trouble! Better get home quick!"
-  end
-  if time < 11
-    return "Keep having fun!"
-  end 
-end
-complex_curfew_checker(11)
+# def complex_curfew_checker(time)
+#   if time == 11
+#     puts "Time to apparate!"
+#   end 
+#   if time > 11
+#     puts "You're in trouble! Better get home quick!"
+#   end
+#   if time < 11
+#     return "Keep having fun!"
+#   end 
+# end
+# complex_curfew_checker(11)
 
-def deluxe_curfew_checker(time)
-    if time == 11
-    puts "Time to apparate!"
-  end 
-  if time > 11
-    puts "You're in trouble! Better get home quick!"
-  end 
-  if time == 9
-      puts "You have 2 hour(s) left to keep having fun!"
-  end
-end
+# def deluxe_curfew_checker(time)
+#     if time == 11
+#     puts "Time to apparate!"
+#   end 
+#   if time > 11
+#     puts "You're in trouble! Better get home quick!"
+#   end 
+#   if time == 9
+#       puts "You have 2 hour(s) left to keep having fun!"
+#   end
+# end
 # deluxe_curfew_checker(9)
 
 # def platinum_curfew_checker(current_time, curfew_time)
